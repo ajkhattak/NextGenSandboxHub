798985dc50261d497d5d925bc14b9c54560603e0
Ahmad Jan Khattak <ajkhattak@gmail.com>
2026-03-03 00:48:11 -0500

    install libraries file clean up.

--- src/R/install_load_libs.R
+++ src/R/install_load_libs.R
@@ -1,151 +1,128 @@
 ######################### INSTALL REQUIRED PACKAGES ############################
 # STEP #1: The packages need to run Hydrofabric tools for subsetting basins
 ################################################################################
+#install.packages("renv")
+#renv::init()
 
 if (Sys.info()['sysname'] == "Windows") {
   options(download.file.method = "curl", download.file.extra="-k -L")
 }
 
-if(!requireNamespace("devtools", quietly=TRUE))
-  install.packages("devtools")
-if(!requireNamespace("curl", quietly=TRUE))
-  install.packages("curl")
-if(!requireNamespace("usethis", quietly=TRUE))
-  install.packages("usethis")
 
-suppressPackageStartupMessages({
-  library(curl)
-  library(usethis)
-  library(devtools)
-})
+cran_packages <- c(
+  "whitebox", "Metrics", "dplyr", "glue", "raster",
+  "jsonlite", "ggplot2", "pbapply", "yaml",
+  "dataRetrieval", "exactextractr", "sf", "terra"
+)
 
-if(!requireNamespace("hydrofabric3D", quietly=TRUE))
-  devtools::install_github("mikejohnson51/hydrofabric3D")
+github_packages <- c(
+  "owp-spatial/hfsubsetR",
+  "mikejohnson51/zonal"
+)
 
-if(!requireNamespace("hydrofabric", quietly=TRUE)) {
-  #devtools::install_github("noaa-owp/hydrofabric", ref = 'b07c109', force = TRUE)
-  devtools::install_github("noaa-owp/hydrofabric", force = TRUE)
+
+installed <- rownames(installed.packages())
+
+for (pkg in cran_packages) {
+  if (!pkg %in% installed) {
+    install.packages(pkg, dependencies = TRUE)
+  }
 }
 
-if(!requireNamespace("climateR", quietly=TRUE)) 
-  devtools::install_github("mikejohnson51/climateR", force = TRUE)
+if (!requireNamespace("remotes", quietly = TRUE)) {
+  install.packages("remotes")
+}
 
-if(!requireNamespace("zonal", quietly=TRUE))
-  devtools::install_github("mikejohnson51/zonal", force = TRUE)
+for (repo in github_packages) {
+  pkg <- basename(repo)
+  if (!requireNamespace(pkg, quietly = TRUE)) {
+    remotes::install_github(repo, upgrade = "never")
+  }
+}
 
-if(!requireNamespace("AOI", quietly=TRUE))
-  devtools::install_github("mikejohnson51/AOI")
-
-if(!requireNamespace("hfsubsetR", quietly=TRUE))
-  remotes::install_github("owp-spatial/hfsubsetR")
-
-if(!requireNamespace("sf", quietly=TRUE)) 
-  install.packages("sf")
-
-if(!requireNamespace("terra", quietly=TRUE)) 
-  install.packages("terra")
-
-if(!requireNamespace("exactextractr", quietly=TRUE)) 
-  install.packages("exactextractr")
-
-if(!requireNamespace("whitebox", quietly=TRUE)) {
-  install.packages("whitebox")
+if (!whitebox::check_whitebox_binary()) {
   whitebox::install_whitebox()
 }
 
-if(!requireNamespace("Metrics", quietly=TRUE)) 
-  install.packages("Metrics")
+suppressPackageStartupMessages({
+  library(zonal)
+  library(whitebox)
+  library(sf)
+  library(terra)
+  library(exactextractr)
+  library(dplyr)
+  library(glue)
+  library(raster)
+  library(jsonlite)
+  library(ggplot2)
+  library(Metrics)
+  library(pbapply)
+  library(parallel)
+  library(yaml)
+  library(dataRetrieval)
+})
 
-if(!requireNamespace("dplyr", quietly=TRUE)) 
-  install.packages("dplyr")
-
-if(!requireNamespace("glue", quietly=TRUE)) 
-  install.packages("glue")
-
-if(!requireNamespace("raster", quietly=TRUE)) 
-  install.packages("raster")
-
-if(!requireNamespace("jsonlite", quietly=TRUE)) 
-  install.packages("jsonlite")
-
-if(!requireNamespace("ggplot2", quietly=TRUE)) 
-  install.packages("ggplot2")
-
-if(!requireNamespace("pbapply", quietly=TRUE)) 
-  install.packages("pbapply")
-
-if(!requireNamespace("yaml", quietly=TRUE)) 
-  install.packages("yaml")
-
-if(!requireNamespace("dataRetrieval", quietly=TRUE)) 
-  install.packages("dataRetrieval")
-
-# install arrow package from source, if any conflicts/errors happen due to arrow package
-if(!requireNamespace("arrow", quietly=TRUE)) {
-  # 1
-  #Sys.setenv("NOT_CRAN" = "true")
-  #Sys.setenv(LIBARROW_BINARY = TRUE)
-  #install.packages('arrow', type = "source") 
-  
-  # 2
-  Sys.setenv(NOT_CRAN=TRUE, LIBARROW_MINIMAL=FALSE, LIBARROW_BINARY=FALSE)
-  install.packages("arrow", repos = "https://packagemanager.posit.co/cran/2024-04-01")
-
-  # 3
-  # if ran into arrow installation issues, once can try the following too.
-  # S3 support not enabled for r-universe by default (https://github.com/apache/arrow/issues/43030)
-  #install.packages("arrow", repos = 'https://apache.r-universe.dev') 
-}
- 
-
-#library(hydrofabric)
-suppressPackageStartupMessages(library(hydrofabric))
-library(climateR)
-library(zonal)
-library(whitebox)
-library(sf)
-library(terra)
-library(exactextractr)
-library(dplyr)
-suppressPackageStartupMessages(library(glue))
-#library(raster)
-suppressPackageStartupMessages(library(raster))
-library(jsonlite)
-library(ggplot2)
-library(Metrics)
-suppressPackageStartupMessages(library(arrow))
-library(pbapply)
-library(parallel)
-library(yaml)
-library(dataRetrieval)
-
-# put all packages in a list for the use in parallel execution
-libraries_lst <- c(suppressPackageStartupMessages(library(hydrofabric)),
-                   library(climateR),
-                   library(zonal),
-                   library(whitebox),
-                   library(sf),
-                   library(terra),
-                   library(dplyr),
-                   suppressPackageStartupMessages(library(glue)),
-                   suppressPackageStartupMessages(library(raster)),
-                   library(jsonlite),
-                   library(ggplot2),
-                   library(Metrics),
-                   suppressPackageStartupMessages(library(arrow)),
-                   library(RSQLite),
-                   library(DBI),
-                   library(pbapply),
-                   library(parallel),
-                   library(yaml)
-                   )
-
-# to sync hydrofabric to local machine
-#local   <- "/Users/ahmadjan/Core/SimulationsData/preprocessing/hydrofabricS3"
-#s3      <- "s3://lynker-spatial/hydrofabric"
-#version <-  'v2.1.1'
-#type    <- "nextgen"
-#domain  <- "conus"
-
-#(sys <- glue::glue("aws s3 sync {s3}/{version}/{type} {local}/{version}/{type}"))
-
+###########
+# if(!requireNamespace("remotes", quietly=TRUE))
+#   install.packages("remotes")
+# 
+# if(!requireNamespace("hfsubsetR", quietly=TRUE))
+#   remotes::install_github("owp-spatial/hfsubsetR")
+# 
+# if(!requireNamespace("zonal", quietly=TRUE))
+#   remotes::install_github("mikejohnson51/zonal")
+# 
+# if(!requireNamespace("whitebox", quietly=TRUE)) {
+#   install.packages("whitebox")
+#   whitebox::install_whitebox()
+# }
+# 
+# if(!requireNamespace("Metrics", quietly=TRUE))
+#   install.packages("Metrics")
+# 
+# if(!requireNamespace("dplyr", quietly=TRUE))
+#    install.packages("dplyr")
+# 
+# 
+# if(!requireNamespace("glue", quietly=TRUE)) {
+#   install.packages("glue")
+# }
+#   
+# 
+# if(!requireNamespace("raster", quietly=TRUE))
+#    install.packages("raster")
+# 
+# if(!requireNamespace("jsonlite", quietly=TRUE))
+#    install.packages("jsonlite")
+# 
+# if(!requireNamespace("ggplot2", quietly=TRUE))
+#    install.packages("ggplot2")
+# 
+# if(!requireNamespace("pbapply", quietly=TRUE))
+#    install.packages("pbapply")
+# 
+# if(!requireNamespace("yaml", quietly=TRUE))
+#    install.packages("yaml")
+# 
+# if(!requireNamespace("dataRetrieval", quietly=TRUE)) 
+#    install.packages("dataRetrieval")
+# 
+# if(!requireNamespace("exactextractr", quietly=TRUE)) 
+#   install.packages("exactextractr")
+# 
+# library(zonal)
+# library(whitebox)
+# library(sf)
+# library(terra)
+# library(exactextractr)
+# library(dplyr)
+# suppressPackageStartupMessages(library(glue))
+# library(raster)
+# suppressPackageStartupMessages(library(raster))
+# library(jsonlite)
+# library(ggplot2)
+# library(Metrics)
+# library(pbapply)
+# library(parallel)
+# library(yaml)
+# library(dataRetrieval)
