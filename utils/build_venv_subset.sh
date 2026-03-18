e0b89a586a768af585650c43d5c6b1a0a9f35b4d
Ahmad Jan Khattak <ajkhattak@gmail.com>
2026-03-17 23:36:41 -0400

    updated subset build script.

--- utils/build_venv_subset.sh
+++ utils/build_venv_subset.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 # Load modules
-#module load rdhpcs-conda/25.3.1
+#module load conda/25.3.1
 
 # Disable conda plugins (faster and avoids conflicts on HPC)
 export CONDA_NO_PLUGINS=true
@@ -10,43 +10,43 @@ export CONDA_NO_PLUGINS=true
 # initialize conda/mamba for non-interactive shell
 eval "$(conda shell.bash hook)"
 
-echo "Using SANDBOX_DIR=${SANDBOX_DIR}"
+echo "Using SANDBOX_BUILD_DIR=${SANDBOX_BUILD_DIR}"
 
 
 # Configure conda directories
-mkdir -p "${SANDBOX_DIR}/rvenv/conda_envs"
-mkdir -p "${SANDBOX_DIR}/rvenv/conda_pkgs"
+mkdir -p "${SANDBOX_BUILD_DIR}/rvenv/conda_envs"
+mkdir -p "${SANDBOX_BUILD_DIR}/rvenv/conda_pkgs"
 
-conda config --add envs_dirs "${SANDBOX_DIR}/rvenv/conda_envs"
-conda config --add pkgs_dirs "${SANDBOX_DIR}/rvenv/conda_pkgs"
+conda config --add envs_dirs "${SANDBOX_BUILD_DIR}/rvenv/conda_envs"
+conda config --add pkgs_dirs "${SANDBOX_BUILD_DIR}/rvenv/conda_pkgs"
 
 
 # Install mamba environment manager
 
-if [ ! -d "${SANDBOX_DIR}/rvenv/mamba" ]; then
-conda create -y -p "${SANDBOX_DIR}/rvenv/mamba" -c conda-forge mamba
+if [ ! -d "${SANDBOX_BUILD_DIR}/rvenv/mamba" ]; then
+conda create -y -p "${SANDBOX_BUILD_DIR}/rvenv/mamba" -c conda-forge mamba
 fi
 
 # Enable mamba in this shell
 
-eval "$("${SANDBOX_DIR}/rvenv/mamba/bin/mamba" shell hook --shell bash)"
+eval "$("${SANDBOX_BUILD_DIR}/rvenv/mamba/bin/mamba" shell hook --shell bash)"
 
-mamba activate "${SANDBOX_DIR}/rvenv/mamba"
+mamba activate "${SANDBOX_BUILD_DIR}/rvenv/mamba"
 
 # Create main R environment
 
-if [ ! -d "${SANDBOX_DIR}/rvenv/venv_subset" ]; then
-mamba env create -y -p "${SANDBOX_DIR}/rvenv/venv_subset" -f "${SANDBOX_DIR}/utils/venv/venv_subset.yaml"
+if [ ! -d "${SANDBOX_BUILD_DIR}/rvenv/venv_subset" ]; then
+mamba env create -y -p "${SANDBOX_BUILD_DIR}/rvenv/venv_subset" -f "${SANDBOX_DIR}/utils/venv/venv_subset.yaml"
 fi
 
 # Activate main environment
-mamba activate "${SANDBOX_DIR}/rvenv/venv_subset"
+mamba activate "${SANDBOX_BUILD_DIR}/rvenv/venv_subset"
 
 
 # Install additional R packages via conda
 
 mamba install -y \
--p "${SANDBOX_DIR}/rvenv/venv_subset" \
+-p "${SANDBOX_BUILD_DIR}/rvenv/venv_subset" \
 -c conda-forge \
 r-rcpp r-gifski r-sourcetools r-rnaturalearth \
 r-later r-promises r-httpuv r-shiny r-leaflet \
