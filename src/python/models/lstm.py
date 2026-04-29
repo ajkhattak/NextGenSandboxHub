8b300fff242e3364809c9b06679adb3634b42c22
Ahmad Jan Khattak <ajkhattak@gmail.com>
2026-04-29 18:32:50 -0400

    refactored workflow to support ensemble lstm

--- src/python/models/lstm.py
+++ src/python/models/lstm.py
@@ -33,91 +33,110 @@ class LSTMConfigurationGenerator(ConfigurationGenerator):
 
         if not os.path.exists(lstm_basefile):
             raise FileNotFoundError(
-                f"Sample LSTM config file does not exist: {lstm_basefile}"
+                f"LSTM base config file does not exist: {lstm_basefile}"
             )
 
         with open(lstm_basefile, "r") as f:
             base_file = yaml.safe_load(f)
 
-        train_cfg_path = os.path.normpath(
-            os.path.join(
-                self.ctx.sandbox_dir,
-                "extern",
-                "lstm",
-                base_file.get("train_cfg_file"),
-            )
-        )
-
-        attributes_file = base_file.get("attributes_file")
-        df_attr_div     = pd.read_parquet(attributes_file)
-        df_attr_div     = df_attr_div.set_index("divide_id")
+        train_cfg_files  = base_file.get("train_cfg_file", [])
+        attributes_files = base_file.get("attributes_file", [])
         
-        with open(train_cfg_path, "r") as f:
-            train_cfg = yaml.safe_load(f)
+        if isinstance(train_cfg_files, str):
+            train_cfg_files = [train_cfg_files]
             
-        static_attrs_lstm_training = train_cfg['static_attributes']
-        
+        if isinstance(attributes_files, str):
+            attributes_files = [attributes_files]
+            
+        if len(train_cfg_files) != len(attributes_files):
+            raise ValueError(
+                "train_cfg_file and attributes_file must have the same length"
+            )
+
+        ## mappings
         static_attributes_cfg = base_file.get("static_attributes", {})
-        static_attrs_parquet_mapping = static_attributes_cfg.get("training", {})
+        static_attrs_parquet_mapping = static_attributes_cfg.get("training", {}) # mapping between training attrs and names in the parquet file
         static_attrs_bmi_mapping = static_attributes_cfg.get("bmi", {})
+    
+        config_ensemble = [] # lstm ensemble using different training weights
 
-        assert set(static_attrs_lstm_training) == set(static_attrs_parquet_mapping.keys())
+        for train_f, attr_f in zip(train_cfg_files, attributes_files):
 
-        assert all(col in df_attr_div.columns
-                   for col in static_attrs_parquet_mapping.values()
-                   ), "Some mapped attribute columns are missing in parquet!"
+            train_path = os.path.normpath(os.path.join(self.ctx.sandbox_dir, "extern", "lstm", train_f))
+
+            attr_path = os.path.normpath(os.path.join(self.ctx.sandbox_dir, attr_f))
+
+            if not os.path.exists(train_path):
+                raise FileNotFoundError(f"Missing attributes file: {train_path}")
+
+            # Load training config
+            with open(train_path, "r") as f:
+                train_cfg = yaml.safe_load(f)
+
+            static_attrs_training = train_cfg.get("static_attributes", [])
+
+            # Load attributes parquet
+            if not os.path.exists(attr_path):
+                raise FileNotFoundError(f"Missing attributes file: {attr_path}")
+
+            df = pd.read_parquet(attr_path).set_index("divide_id")
+
+            config_ensemble.append({
+                "train_cfg_path": train_path,
+                "static_attrs_parquet": df,
+                "static_attrs_training": static_attrs_training
+            })
+
 
         gpkg_name = os.path.basename(self.ctx.gpkg_file).split(".")[0]
         gage_id = gpkg_name.split("_")[1]
+        
 
         for catID in self.ctx.catids:
             cat_name = f"cat-{catID}"
             
             fname_lstm = f'lstm_{tag}_{cat_name}.yaml'
             lstm_file = os.path.join(lstm_dir, fname_lstm)
+
+            config = {
+                "train_cfg_file": [c["train_cfg_path"] for c in config_ensemble],
+                "basin_id": gage_id,
+                "verbose": 0,
+                "time_step": "1 hour",
+                "initial_state": "zero",
+                "static_attributes": {}
+            }
+
+            for member in config_ensemble:
+                df = member["static_attrs_parquet"]
+                attrs = member["static_attrs_training"]
             
-            if cat_name not in df_attr_div.index:
-                raise KeyError(f"{cat_name} not found in attributes parquet")
+                if cat_name not in df.index:
+                    raise KeyError(f"{cat_name} not found in attributes parquet")
+            
+                for attr in attrs:
+                    if attr not in static_attrs_parquet_mapping:
+                        print(f"Warning: {attr} missing in mapping, skipping")
+                        continue
 
-            centroid = self.ctx.gdf.loc[cat_name, "geometry"].centroid
+                    parquet_col = static_attrs_parquet_mapping[attr]
 
-            config = {
-                "train_cfg_file": train_cfg_path,
-                "basin_id": gage_id,
-                "verbose": 0,
-                "time_step": "1 hour",
-                "initial_state": "zero",
-                "static_attributes": {}   # initialize
-            }
+                    if parquet_col not in df.columns:
+                        raise ValueError(f"Missing column {parquet_col} in parquet")
 
-            # Add training attributes
-            for lstm_name, parquet_col in static_attrs_parquet_mapping.items():
-                config["static_attributes"][lstm_name] = float(df_attr_div.loc[cat_name][parquet_col])
-
-            # Add BMI attributes
+                    config["static_attributes"][attr] = float(df.loc[cat_name][parquet_col])
+                
             for bmi_name, parquet_col in static_attrs_bmi_mapping.items():
-                config[bmi_name] = float(df_attr_div.loc[cat_name][parquet_col])
+                found = False
+                for member in config_ensemble:
+                    df = member["static_attrs_parquet"]
+                    if parquet_col in df.columns and cat_name in df.index:
+                        config[bmi_name] = float(df.loc[cat_name][parquet_col])
+                        found = True
+                        break
 
-
-            """
-            config = {
-                "train_cfg_file": train_cfg_path,
-                "basin_id": gage_id,
-                "verbose": 0,
-                "time_step": "1 hour",
-                "initial_state": "zero",
-                "ari_ix_mean" : float(df_attr_div.loc[cat_name]["ari_ix_mean"]),
-                "slp_dg_mean" : float(df_attr_div.loc[cat_name]["slp_dg_mean"]),
-                "centroid_y"  : float(df_attr_div.loc[cat_name]["lat_dec_deg"]),
-                "ims_pc_mean" : float(df_attr_div.loc[cat_name]["ims_pc_mean"]),
-                "ele_mt_mean" : float(df_attr_div.loc[cat_name]["ele_mt_mean"]),
-                "snd_pct"     : float(df_attr_div.loc[cat_name]["snd_pct"]),
-                "cly_pct"     : float(df_attr_div.loc[cat_name]["cly_pct"]),
-                "lka_pc_sse"  : float(df_attr_div.loc[cat_name]["lka_pc_sse"]),
-                "areasqkm"    : float(df_attr_div.loc[cat_name]["areasqkm"]),
-                "for_pc_forest_sse" : float(df_attr_div.loc[cat_name]["for_pc_forest"]),
-            }
-            """
+                if not found:
+                    raise ValueError(f"BMI column {parquet_col} not found in any attributes file")
 
             with open(lstm_file, "w") as f:
                 yaml.dump(config, f, sort_keys=False)
